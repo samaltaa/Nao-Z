53f072dadfa2cf2f9cc7a3c422213deacb4fcd3d
Samira <samirasanchez8090@gmail.com>
2023-08-27 23:55:31 -0400

    card component folder setup, header style

---
+++ Frontend/src/app/Components/Cards/WordCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//typescript interface
+interface WordCardProps {
+    term: string;
+    definition: string;
+    pronunciation: string;
+}
+
+const WordCard = () => {
+  return (
+    <div>WordCard</div>
+  )
+}
+
+export default WordCard
--- Frontend/src/app/Components/Header.jsx
+++ Frontend/src/app/Components/Header.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <nav>
-            <div>
-                <a>
-                    <img/>
+        <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800'>
+            <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+                <a href='#'
+                    className='flex items-center'>
+                    <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>Nao Z</span>
                 </a>
-                <div>
-                    <a>Log In</a>
-                    <a>Get Started</a>
+                <div className='flex items-center lg:order-2'>
+                    <a className='text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800'>Log In</a>
+                    <a className='text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800'>Get Started</a>
                 </div>
                 <div>
                     <ul>
