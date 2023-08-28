95f1c5928895c1256670ba39b6e792c660d95e1a
Samira <samirasanchez8090@gmail.com>
2023-08-27 21:46:01 -0400

    header component skeletal code

---
+++ Frontend/src/app/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <div>
+                <a>
+                    <img/>
+                </a>
+                <div>
+                    <a>Log In</a>
+                    <a>Get Started</a>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <a>Courses</a>
+                        </li>
+                        <li>
+                            <a>Search</a>
+                        </li>
+                        <li>
+                            <a>Create Course</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
