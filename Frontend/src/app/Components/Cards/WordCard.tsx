a938739bf97bebe0f26b02354f319029369a7653
Samira <samirasanchez8090@gmail.com>
2023-08-28 10:59:46 -0400

    wordcard component styled

--- Frontend/src/app/Components/Cards/WordCard.tsx
+++ Frontend/src/app/Components/Cards/WordCard.tsx
@@ -7,10 +7,22 @@ interface WordCardProps {
     pronunciation: string;
 }
 
-const WordCard = () => {
+const WordCard = ({ term, definition, pronunciation}: WordCardProps) => {
   return (
-    <div>WordCard</div>
-  )
+    <div className='w-full max-w-md mx-auto bg-lavender-light rounded-md shadow:md'>
+        <div className='p-4'>
+            <h2 className='text-xl font-bold text-lavender-dark'>
+                {term}
+            </h2>
+            <p className='text-gray-500 mb-4'>
+                {definition}
+            </p>
+            <p className='text-gray-500'>
+                Pronunciation: {pronunciation}
+            </p>
+        </div>
+    </div>
+  );
 }
 
 export default WordCard
