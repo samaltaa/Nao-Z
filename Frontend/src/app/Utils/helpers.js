a0117bfde317c5839e9d2f9662cf0d9c73f00449
Samira <samirasanchez8090@gmail.com>
2023-08-31 12:23:14 -0400

    documentation of helpers.js

--- Frontend/src/app/Utils/helpers.js
+++ Frontend/src/app/Utils/helpers.js
@@ -1,8 +1,15 @@
-
-
-/* cross-component data fetching function for API endpoint
-    connection to frontend 
+/* this file contains all the functions that will be used multiple times 
+    across the different component of the program.
+    if you are going to make any changes to this files, 
+    make sure that you are in the 'refactor' branch 
+    and carefully document the changes and test the changes
+    prior to committing the code and merging it to main branch
+    to avoud breaking the entire site. :)
 */
+
+
+
+//data fetching function for API endpoint connection to frontend 
 export async function fetchData(url) {
     try {
       const response = await fetch(url);
